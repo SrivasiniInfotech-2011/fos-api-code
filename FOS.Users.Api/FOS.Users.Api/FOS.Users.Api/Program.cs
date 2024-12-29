221abec052a4d8834083c5b8fcdd3007fe2023cb
Srivatsan S <seshadri.srivatsan@gmail.com>
2024-12-29 22:18:42 +0530

    completed the User Image Upload

--- FOS.Users.Api/FOS.Users.Api/FOS.Users.Api/Program.cs
+++ FOS.Users.Api/FOS.Users.Api/FOS.Users.Api/Program.cs
@@ -1,5 +1,7 @@
 using FOS.Infrastructure.Commands;
 using FOS.Infrastructure.Queries;
+using FOS.Infrastructure.Services.FileServer;
+using FOS.Models.Configurations;
 using FOS.Models.Constants;
 using FOS.Models.Entities;
 using FOS.Models.Requests;
@@ -40,7 +42,7 @@ builder.Services.AddAuthentication(IdentityServerAuthenticationDefaults.Authenti
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
-        builder => builder.WithOrigins(configuration["AllowCORSUrls"]!.Split(',') )
+        builder => builder.WithOrigins(configuration["AllowCORSUrls"]!.Split(','))
                           .AllowAnyMethod()
                           .AllowAnyHeader()
                           .AllowCredentials());
@@ -63,15 +65,15 @@ builder.Services.AddTransient<IMediator, Mediator>();
 builder.Services.AddTransient<IUserRepository>(s => new UserRepository(configuration.GetConnectionString("FOSConnectionString")!));
 builder.Services.AddTransient<IRequestHandler<GetUserByUserNameAndPassword.Query, User?>, GetUserByUserNameAndPassword.Handler>();
 builder.Services.AddTransient<IRequestHandler<GetUserMenusByUserId.Query, List<UserMenu>>, GetUserMenusByUserId.Handler>();
-
+builder.Services.AddTransient<IFileServerService, FileServerService>(s => new FileServerService(new FileServerConfiguration { CmsFilePath = configuration["CmsPath"], CmsUrl = configuration["CmsUrl"] }, s.GetService<ILogger<FileServerService>>()!));
 builder.Services.AddTransient<IUsermanagementRepository>(s => new UserManagementRepository(configuration.GetConnectionString("FOSConnectionString")!));
 builder.Services.AddTransient<IUsermanagementRepository>(s => new UserManagementRepository(configuration.GetConnectionString("FOSConnectionString")!));
 builder.Services.AddTransient<IRequestHandler<UserLevelLookup.Query, List<Lookup>>, UserLevelLookup.Handler>();
 builder.Services.AddTransient<IRequestHandler<GetUserDesignationlevelLookups.Query, List<Lookup>>, GetUserDesignationlevelLookups.Handler>();
 builder.Services.AddTransient<IRequestHandler<ViewUserDetails.Query, GetInsertUserDetailsModel>, ViewUserDetails.Handler>();
 //builder.Services.AddTransient<IRequestHandler<GetUsertranslanderInfrastructure.Query, GetUserTranslanderModel>, GetUsertranslanderInfrastructure.Handler>();
-builder.Services.AddTransient<IRequestHandler<GetUsertranslanderInfrastructure.Query, List <GetUserTranslanderModel>>, GetUsertranslanderInfrastructure.Handler>();
-builder.Services.AddTransient<IRequestHandler<UserReportinglevel.Query, List <ReportingLevel>>, UserReportinglevel.Handler>();
+builder.Services.AddTransient<IRequestHandler<GetUsertranslanderInfrastructure.Query, List<GetUserTranslanderModel>>, GetUsertranslanderInfrastructure.Handler>();
+builder.Services.AddTransient<IRequestHandler<UserReportinglevel.Query, List<ReportingLevel>>, UserReportinglevel.Handler>();
 builder.Services.AddTransient<IRequestHandler<InsertUserDetails.Command, int>, InsertUserDetails.Handler>();
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
